ef45ae57fc7fa289098a8ae01be060c7f9485e33
Jeriko3 <david.buono@protonmail.com>
2025-08-29 15:53:18 +0200

    Added ignore for empty lines in input url txt

--- src/SongsFileReader.py
+++ src/SongsFileReader.py
@@ -7,7 +7,8 @@ class SongsFileReader:
         with open(path, "r") as songs_to_download_file:
             for url_line in songs_to_download_file:
                 url_line = url_line.replace("\n","")
-                self.song_url_list.append(url_line)
+                if(url_line != ""): #IGNORE EMPTY LINES
+                    self.song_url_list.append(url_line)
 
     def get_url_list(self):
         return self.song_url_list
